7840acf3e6bf30eeeab8c53f49ad620f4bbc70e3
Luc Baumeler <lucbaumeler@MacBook-Pro-von-Luc.local>
2025-02-24 07:56:07 +0100

    m

--- ImgToTrumpf.py
+++ ImgToTrumpf.py
@@ -160,7 +160,10 @@ def predict():
     except Exception as e:
         os.remove(temp_filename)
         return jsonify({'error': str(e)})
+
+application = app
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    application.run(host='0.0.0.0', port=port)
         
